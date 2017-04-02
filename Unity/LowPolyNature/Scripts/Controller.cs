e84ec8c2bb94bfbf7e23fe25d69db7b2f40f9e5b
Shun Yao <shunyaorad@gmail.com>
2017-04-02 12:16:14 -0400

    added Unity Scripts

---
+++ Unity/LowPolyNature/Scripts/Controller.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Controller : MonoBehaviour {
+
+	public float speed;
+	public float rotationalSpeed;
+
+	private string status;
+	private Vector3 spawnPoint;
+	public WWW command;
+	public string commandURL = "http://www.contrib.andrew.cmu.edu/~ashishb/cgi-bin/read.php?user=test";
+
+	public Queue<string> commandQue = new Queue<string> (); // collects all the commands 
+
+	public int numCopy; // number of repeated commands
+
+	// Use this for initialization
+	void Start () 
+	{
+		status = "stop";
+		spawnPoint = transform.position;
+		StartCoroutine(ConnectWeb());
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (commandQue.Count != 0) 
+		{
+			string currStatus = commandQue.Dequeue();
+			Debug.Log ("Current command" + currStatus);
+
+			if (currStatus == "up") 
+			{
+				transform.position = transform.position + Camera.main.transform.forward * speed * Time.deltaTime;
+			}
+
+			if (currStatus == "down") 
+			{
+				transform.position = transform.position - Camera.main.transform.forward * speed * Time.deltaTime;
+			}
+			if (currStatus == "left") 
+			{
+				//transform.Rotate (0, -rotationalSpeed * Time.deltaTime, 0, Space.World);
+				transform.position = transform.position + Quaternion.Euler(0, -90, 0) * Camera.main.transform.forward * speed * Time.deltaTime;
+			}
+
+			if (currStatus == "right") 
+			{
+				//transform.Rotate (0, rotationalSpeed * Time.deltaTime, 0, Space.World);
+				transform.position = transform.position + Quaternion.Euler(0, 90, 0) * Camera.main.transform.forward * speed * Time.deltaTime;
+
+			}
+			
+		}
+
+		// if user fall from the ground, return to original position
+		if (transform.position.y < -10f) 
+		{
+			transform.position = spawnPoint;
+		}
+	}
+
+	public IEnumerator ConnectWeb ()
+	{
+		while (true) 
+		{
+			//Debug.Log ("Connect Web running");
+			command = new WWW (commandURL);
+
+			StartCoroutine (WaitForRequest (command));
+			// do nothing until json is loaded
+			// while (!command.isDone) {} // this line slows down FPS
+			// deltaTime = Time.deltaTime;
+			yield return command;
+			StartCoroutine(GetCommandQue());
+		}
+	}
+
+	public IEnumerator WaitForRequest (WWW www)
+	{
+		yield return www;
+	}
+
+	public IEnumerator GetCommandQue()
+	{
+		while (true) 
+		{
+			//Debug.Log ("GetCommandQue Running");
+
+			if (command.text == "")
+			{
+				//Debug.Log ("Null command");
+				yield return commandQue; // this part prevents error WWW not ready and saves FPS!
+			}
+
+			if (command != null && command.text != "" && 
+				!command.text.StartsWith ("<") && (command.error == null || command.error == "")) 
+			{
+				JSONObject tempData = new JSONObject (command.text);
+
+				for (int i = 0; i < tempData.list.Count; i++) 
+				{
+					JSONObject input = (JSONObject)tempData.list [i];
+
+					if (input == null) 
+					{
+						status = "stop";
+					} else 
+					{
+						status = input ["Action"].str;
+					}
+					for (int copy = 0; copy < numCopy; copy++) 
+					{
+						commandQue.Enqueue (status);
+					}
+				}
+			}
+			yield return commandQue;
+		}
+	}
+}
+	
---
+++ Unity/LowPolyNature/Scripts/MicrophoneInput.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicrophoneInput : MonoBehaviour {
+
+	public float minThreshold = 0;
+	public float frequency = 0.0f;
+	public int audioSampleRate = 44100;
+	public string microphone;
+
+	private List<string> options = new List<string>();
+	private AudioSource audioSource;
+
+	public Light lt;
+	public float duration = 6000.0F; // duration of light change back to initial
+	private float initialIntensity;
+	public float dimLight;
+
+	private bool dimTriggered;
+
+	// Use this for initialization
+	void Start () {
+		initialIntensity = lt.intensity; // set initial intensity
+		dimTriggered = false;
+		//get components you'll need
+		audioSource = GetComponent<AudioSource> ();
+
+		// get all available microphones
+		foreach (string device in Microphone.devices) {
+			if (microphone == null) {
+				//set default mic to first mic found.
+				microphone = device;
+			}
+			options.Add(device);
+		}
+
+		UpdateMicrophone ();
+	}
+	void UpdateMicrophone(){
+		audioSource.Stop(); 
+		//Start recording to audioclip from the mic
+		audioSource.clip = Microphone.Start(microphone, true, 10, audioSampleRate);
+		audioSource.loop = true; 
+		// Mute the sound with an Audio Mixer group becuase we don't want the player to hear it
+		Debug.Log(Microphone.IsRecording(microphone).ToString());
+
+		if (Microphone.IsRecording (microphone)) { //check that the mic is recording, otherwise you'll get stuck in an infinite loop waiting for it to start
+			while (!(Microphone.GetPosition (microphone) > 0)) {
+			} // Wait until the recording has started. 
+
+			Debug.Log ("recording started with " + microphone);
+
+			// Start playing the audio source
+			audioSource.Play (); 
+		} else {
+			//microphone doesn't work for some reason
+
+			Debug.Log (microphone + " doesn't work!");
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float audio = audioSource.GetSpectrumData(128,0,FFTWindow.BlackmanHarris)[64]*1000000;
+		if (audio > minThreshold && !dimTriggered) 
+		{
+			Debug.Log ("***** Audio Volume: " + audio + "*******");
+			lt.intensity = dimLight;
+			dimTriggered = true; // prevent dimming light while light is dim
+		}
+		// gradually return to initial light intensity
+		if (lt.intensity < initialIntensity) 
+		{
+			float phi = Time.time / duration * 2 * Mathf.PI;
+			float amplitude = Mathf.Cos (phi) * (initialIntensity - dimLight) + dimLight;
+			lt.intensity = amplitude;
+			if (lt.intensity > initialIntensity - 0.05F) 
+			{
+				lt.intensity = initialIntensity;
+				dimTriggered = false;
+			}
+		}
+	}
+}
